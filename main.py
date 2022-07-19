534b69c06d006260f00c0f765ab5e6933b25fcc6
killgriff22 <kilgrif22033@gmail.com>
2022-07-19 06:00:40 -0700

    getting tiles working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import pygame,assetloader
-WIDTH = 360
-HEIGHT = 480
+tilesize=50
+WIDTH = tilesize*20
+HEIGHT = tilesize*10
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -11,15 +12,23 @@ assets = assetloader.loadassets()
 print((assets.sprites))
 if assets.ICON:
     pygame.display.set_icon(assets.ICON)
-def drawcell(x, y,tile:(pygame.surface.Surface)):
+def drawcell(x:int, y:int,tile:(pygame.surface.Surface)) -> None:
     screen.blit(tile,(x,y))
+map = [[[]*30]*30]
 while 1:
     try:
+        screen.fill((255,255,255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        screen.fill((0xFF,0xFF,0xFF))
-        drawcell(10,10,assets.sprites[0][0])
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                if x%tilesize==0:
+                    screen.set_at((x,y),(0,0,0))
+                if y%tilesize==0:
+                    screen.set_at((x,y),(0,0,0))
+                if x%tilesize==0 and y%tilesize==0:
+                    screen.blit(pygame.transform.scale(assets.sprites[0][0],(tilesize,tilesize)),(x,y))
         pygame.display.flip() 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt or pygame.error:
         pygame.quit()      
